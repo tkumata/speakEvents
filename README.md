e275e3df5b5eedcc50d700e177d74c6dfbab684d
Tomokatsu Kumata <tkumata@gmail.com>
2016-04-01 09:46:21 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# speakEvents.py
+## 説明
+Raspberry Pi に BLE やタクトスイッチなどの何かしらのアクションがあった時、iCloud から当日の予定を取得し声でお知らせします。声でお知らせしてくれるので、朝、何かしながら予定の確認をすることができます。
+## 必要なもの
+1. pyicloud
+2. AquesTalkPi
+3. wrapper of AquesTalkPi (eg, atalk.sh)
+## 導入
+1. sudo pip install pyicloud
+2. Download AquesTalkPi and unzip.
+3. vi atalk.sh
+4. Download speakEvent.py (this script).
+5. touch ~/.pyicloud && chmod 600 ~/.pyicloud
+6. vi ~/.pyicloud
+```
+[account]
+user = your apple id
+pass = your apple id password
+```
+7. Adjust speakEvent.py (eg, path etc...)
+## ライセンス
+MIT
+## 著者
+tkumata
---
+++ speakEvents.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from pyicloud import PyiCloudService    # pip install pyicloud
+import datetime
+import locale
+import json
+import subprocess
+import time
+import os
+from ConfigParser import SafeConfigParser
+
+#
+# touch ~/.pyicloud
+# chmod 600 ~/.pyicloud
+# vi ~/.pyicloud
+#[account]
+#user = your appleid
+#pass = your appleid password
+#
+homeDir = os.path.expanduser("~")
+filename = homeDir + '/.pyicloud'
+parser = SafeConfigParser()
+parser.read(filename)
+
+userid = parser.get('account', 'user')
+assert isinstance(userid, str)
+passwd = parser.get('account', 'pass')
+assert isinstance(passwd, str)
+
+api = PyiCloudService(userid, passwd)
+d = datetime.datetime.today()
+
+def get_iccdata():
+    from_dt = datetime.datetime(d.year, d.month, d.day)
+    to_dt = datetime.datetime(d.year, d.month, d.day)
+    iccEvent = api.calendar.events(from_dt, to_dt)
+    return iccEvent
+
+if __name__ == '__main__':
+    events = get_iccdata()
+
+    if len(events) == 0:
+        talk = u"本日の予定はありません"
+        subprocess.call("atalk.sh \"" + talk + "\"", shell=True)
+        quit()
+
+    for event in events:
+        for key,value in event.items():
+            if key == "startDate":
+                if value[4] == 0 and value[5] == 0:
+                    eventTime =  u"終日"
+                else:
+                    eventTime =  str(value[4]) + u"時" + str(value[5]) + u"分から"
+
+                print eventTime
+                subprocess.call("atalk.sh \"" + eventTime + "\"", shell=True)
+
+            if key == "endDate":
+                if value[4] == 0 and value[5] == 0:
+                    pass
+                else:
+                    eventEndTime =  str(value[4]) + u"時" + str(value[5]) + u"分まで"
+                    print eventEndTime
+                    subprocess.call("atalk.sh \"" + eventEndTime + "\"", shell=True)
+
+            if key == "title":
+                eventTitle = value + u"の予定があります"
+                print eventTitle
+                subprocess.call("atalk.sh \"" + eventTitle + "\"", shell=True)
+                time.sleep(2)
