b4b945491008f03274af19df541bee642782346d
Tomokatsu Kumata <tkumata@gmail.com>
2016-04-02 14:20:16 +0900

    sort event

--- speakEvents.py
+++ speakEvents.py
@@ -8,6 +8,7 @@ import subprocess
 import time
 import os
 from ConfigParser import SafeConfigParser
+from pprint import pprint
 
 #
 # touch ~/.pyicloud
@@ -45,31 +46,30 @@ if __name__ == '__main__':
         subprocess.call("atalk.sh -s 120 \"" + talk + "\"", shell=True)
         quit()
     else:
+        events2 = sorted(events, key=lambda x:x['startDate'])
         # 一日分のループ
-        for event in events:
-            # 個別イベント毎のループ
+        for event in events2:
+            # 個別イベントのループ
             for key,value in event.items():
                 if key == "startDate":
                     if value[4] == 0 and value[5] == 0:
                         eventTime =  u"終日"
                     else:
                         eventTime =  str(value[4]) + u"時" + str(value[5]) + u"分から"
-
+                    print eventTime
                     subprocess.call("atalk.sh -s 120 \"" + eventTime + "\"", shell=True)
-
                 if key == "endDate":
                     if value[4] == 0 and value[5] == 0:
                         eventEndTime =  u"に"
                     else:
                         eventEndTime =  str(value[4]) + u"時" + str(value[5]) + u"分まで"
-
                     subprocess.call("atalk.sh -s 120 \"" + eventEndTime + "\"", shell=True)
-
                 if key == "title":
                     eventTitle = value + u"の予定があります"
+                    print eventTitle
                     subprocess.call("atalk.sh -s 100 \"" + eventTitle + "\"", shell=True)
                     time.sleep(1)
-
+            #
         # 一日分のループが終了したら
         endTalk=u"忘れ物はありませんか？　以上。"
         subprocess.call("atalk.sh -s 110 \"" + endTalk + "\"", shell=True)
