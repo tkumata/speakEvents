46966ba7dbbe1bc65430d3d186464b8ecc1db1e8
tkumata <tkumata@gmail.com>
2016-04-27 13:20:59 +0900

    fix.

--- speakEvents.py
+++ speakEvents.py
@@ -14,18 +14,18 @@ import os, sys
 import platform
 import requests, re
 
-#homeDir = os.path.expanduser('~')
-configFile = '/home/pi/.speakevents'
+# Global var
+homeDir = os.path.expanduser('~')
+configFile = homeDir + '/.speakevents'
 lockFile = '/tmp/speakEventsLockfile'
 mplayerLog = '/tmp/speakEventsMpLogfile'
 countButton3 = 0
-
 weatherurl1 = ''
 weatherurl2 = ''
 userid = ''
 passwd = ''
 
-# init Port.
+# Init GrovePi+ ports.
 button2 = 2
 button3 = 3
 #button4 = 4
@@ -33,7 +33,7 @@ grovepi.pinMode(button2, 'INPUT')
 grovepi.pinMode(button3, 'INPUT')
 #grovepi.pinMode(button4, 'OUTPUT')
 
-# check text speaker
+# Check text speaker
 if platform.system() == 'Linux':
     if spawn.find_executable('/home/pi/bin/atalk.sh'):
         speaker = '/home/pi/bin/atalk.sh -s'
@@ -67,7 +67,7 @@ def afn360(channel):
     foundMplayer = 0
     
     # if this code founds mplayer, kill mplayer and turn on flag.
-    psCmd = subprocess.Popen('ps ax', stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True, shell=True)
+    psCmd = subprocess.Popen('ps' 'ax', stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True, shell=True)
     out = psCmd.communicate()[0]
     
     # find mplayer process and kill
@@ -77,7 +77,7 @@ def afn360(channel):
             print('=====> stop AFN.')
             pid = int(line.split(None, 1)[0])
             os.kill(pid, signal.SIGKILL)
-        
+    
     # Play AFN
     if foundMplayer == 0:
         # If not found mplayer, run mplayer.
@@ -95,7 +95,7 @@ def afn360(channel):
     else:
         # If found mplayer, remove mplayer log file.
         os.remove(mplayerLog)
-
+    
     # Remove lock file.
     os.remove(lockFile)
 
@@ -115,73 +115,71 @@ def get_config():
     if os.path.isfile(configFile):
         parser = SafeConfigParser()
         parser.read(configFile)
-
+        
         userid = parser.get('account', 'user')
         assert isinstance(userid, str)
-
+        
         passwd = parser.get('account', 'pass')
         assert isinstance(passwd, str)
-
+        
         weatherurl1 = parser.get('weatherurls', 'weather1')
         assert isinstance(weatherurl1, str)
-
+        
         weatherurl2 = parser.get('weatherurls', 'weather2')
         assert isinstance(weatherurl2, str)
     else:
         print u'=====> config file not found.'
         quit()
-    
+
 # get iCloud Calendar data.
 def get_iccdata():
     api = PyiCloudService(userid, passwd)
-
+    
     if not api == '':
         d = datetime.datetime.today()
         from_dt = datetime.datetime(d.year, d.month, d.day)
         to_dt = datetime.datetime(d.year, d.month, d.day)
-        
         iccEvents = api.calendar.events(from_dt, to_dt)
         
         return iccEvents
     else:
         print('=====> api is null.')
         quit()
-    
+
 # Get Weather Info 1
 def get_weatherinfo1(url):
     req = requests.get(url)
     html = req.text
-
+    
     strPattern = r'<h2 class="sub_title">(.*)</p>'
-    tagPattern = re.compile(r'<[^>]*?>')
-
+    tagPattern = re.compile(r'<[^>]*?>')            # remove html tag pattern
     matches = re.finditer(strPattern, html)
     
     for match in matches:
         w = tagPattern.sub('', match.groups()[0])
-
+    
     return w
 
 # Get Weather Info 2
 def get_weatherinfo2(url):
-    # info : [気温, NN, NN, 降水確率, NN%, NN%, NN%, NN%]
     info = []
-    tagPattern = re.compile(r'<[^>]*?>')
-
-    # Weather HTML
+    tagPattern = re.compile(r'<[^>]*?>')                # remove html tag pattern
+    
+    # Get HTML strings of today's weather
     req = requests.get(url)
     html = req.text
     tmpPattern = ur'今日の天気(.*?)明日の天気'
     match = re.search(tmpPattern, html, re.DOTALL)
     todayHtml = match.group(1)
     
-    # Get temperature.
+    # Get high and low temperature.
     tempPattern = ur'<td class="temp"><span class="bold">(.*)</span>℃</td>'
     matches = re.finditer(tempPattern, todayHtml)
     info.append(u'気温')
     for match in matches:
         tempN = tagPattern.sub('', match.groups()[0])
         info.append(tempN + u'度')
+    # info : [気温, NN度, NN度]
     
     # Get chance of rain.
     rainPattern = ur'<td>(.*)</td>'
@@ -192,8 +190,8 @@ def get_weatherinfo2(url):
         if rainN == '---':
             rainN = u'なし'
         info.append(rainN)
-
-    # info : [気温, 最高気温, NN, 最低気温, NN, 降水確率, 0時から6時, NN%, 6時から12時, NN%, 12時から18時, NN%, 18時から24時, NN%]
+    # info : [気温, NN度, NN度, 降水確率, NN%, NN%, NN%, NN%]
+    
     if len(info) == 8:
         info.insert(1, u'最高気温')
         info.insert(3, u'最低気温')
@@ -203,7 +201,8 @@ def get_weatherinfo2(url):
         info.insert(12, u'18時から24時')
     else:
         info = []
-
+    
+    # info : [気温, 最高気温, NN, 最低気温, NN, 降水確率, 0時から6時, NN%, 6時から12時, NN%, 12時から18時, NN%, 18時から24時, NN%]
     return info
 
 # speak events.
@@ -214,7 +213,7 @@ def speakEvents():
     
     # Get config
     get_config()
-
+    
     # Speak Weather 1
     weatherinfo1 = get_weatherinfo1(weatherurl1)
     if not len(weatherinfo1) == 0:
@@ -236,6 +235,7 @@ def speakEvents():
         subprocess.call(speaker + ' 120 "' + talk + '"', shell=True)
     else:
         events2 = sorted(events, key=lambda x:x['startDate'])    # sort by startDate
+        
         # 一日分のループ
         for event in events2:
             # 個別イベントのループ
@@ -259,8 +259,6 @@ def speakEvents():
                     #print eventTitle
                     subprocess.call(speaker + ' 100 "' + eventTitle + '"', shell=True)
                     time.sleep(1)
-            # End for
-        # End for
         
         # 一日分のループが終了したら
         endTalk = u'忘れ物はありませんか。以上'
